b7a55f9c109def3c1602fe82a044afdf7bb03bc5
Gavin Brown <scareb15@gmail.com>
2019-01-10 00:19:30 -0700

    development files starter adds image titles to array

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var pictures: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let fm = FileManager.default
+        let fm = FileManager.default // allows us to look for files
+        let path = Bundle.main.resourcePath! // finds files added to app
+        let items = try! fm.contentsOfDirectory(atPath: path) // returns contents of path -- is an array with names of all files found in resource directory
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                //this is a picture to load
+                pictures.append(item) // adds photos string to array
+                
+            }
+        }
+        
+        print(pictures)
     }
 
 
